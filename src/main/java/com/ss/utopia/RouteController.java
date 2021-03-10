13dd5ead56c2edee4488352e883ee8ce3f341ccb
SS-christianReyes <christian.reyes@smoothstack.com>
2021-03-10 10:43:47 -0500

    RouteMS Refactoring

--- src/main/java/com/ss/utopia/RouteController.java
+++ src/main/java/com/ss/utopia/RouteController.java
@@ -20,11 +20,11 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,8 +42,8 @@ public class RouteController {
 			: new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
 	}
 	
-	@GetMapping("/id")
-	public ResponseEntity<Object> findById(@RequestParam Integer id){
+	@GetMapping("{id}")
+	public ResponseEntity<Object> findById(@PathVariable Integer id){
 		try {
 			Route route =  routeService.findById(id);
 			return new ResponseEntity<>(route, HttpStatus.OK);
@@ -65,9 +65,9 @@ public class RouteController {
 	@PostMapping
 	public ResponseEntity<Object> insert(@RequestBody HashMap<String, String> routeMap) {
 		try {
-			String origin = routeMap.get("origin");
-			String destination = routeMap.get("destination");
-			Route newRoute = routeService.insert(origin, destination);
+			String routeOriginIataId = routeMap.get("routeOriginIataId");
+			String routeDestinationIataId = routeMap.get("routeDestinationIataId");
+			Route newRoute = routeService.insert(routeOriginIataId, routeDestinationIataId);
 			return new ResponseEntity<>(newRoute, HttpStatus.CREATED);
 
 		}	catch (AirportNotFoundException err) {
@@ -81,10 +81,10 @@ public class RouteController {
 	@PutMapping
 	public ResponseEntity<Object> update(@RequestBody HashMap<String, String> routeMap) {
 		try {
-			Integer id = Integer.parseInt(routeMap.get("routeId"));
-			String origin = routeMap.get("routeOrigin");
-			String destination = routeMap.get("routeDestination");
-			Route newRoute = routeService.update(id, origin, destination);
+			Integer routeId = Integer.parseInt(routeMap.get("routeId"));
+			String routeOriginIataId = routeMap.get("routeOriginIataId");
+			String routeDestinationIataId = routeMap.get("routeDestinationIataId");
+			Route newRoute = routeService.update(routeId, routeOriginIataId, routeDestinationIataId);
 			return new ResponseEntity<>(newRoute, HttpStatus.NO_CONTENT);
 		
 		}	catch (AirportNotFoundException err) {
@@ -98,10 +98,10 @@ public class RouteController {
 		} 
 	}
 	
-	@DeleteMapping
-	public ResponseEntity<Object> delete(@RequestParam Integer id) {
+	@DeleteMapping("{routeId}")
+	public ResponseEntity<Object> delete(@PathVariable Integer routeId) {
 		try {
-			routeService.deleteById(id);
+			routeService.deleteById(routeId);
 			return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
 
 		} catch(RouteNotFoundException err) {
@@ -109,7 +109,7 @@ public class RouteController {
 		}
 	}
 	
-@ExceptionHandler(ConnectException.class)
+	@ExceptionHandler(ConnectException.class)
 	public ResponseEntity<Object> invalidConnection() {
 		return new ResponseEntity<>(new HttpError("Service temporarily unavailabe.", 500), HttpStatus.SERVICE_UNAVAILABLE);
 	}
